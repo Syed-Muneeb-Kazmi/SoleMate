6cb53b48233e159454cee2d833a45319cdb4f517
Syed-Muneeb-Kazmi <kazmisyedmuneeb@gmail.com>
2026-08-31 11:37:55 +0500

    Improved the reset and forgot password functionality with Google password

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,21 +1,18 @@
 // server/utils/sendEmail.js
 const nodemailer = require('nodemailer');
 
-const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
-  await transporter.sendMail({
-    from: `"SoleMate" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
-};
 
 module.exports = sendEmail;
